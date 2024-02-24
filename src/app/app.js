1cde02f91a960f0bacefc25461ccd5224dc6c78d
cheppimdq <cheppimdp@gmail.com>
2024-02-24 02:03:57 -0300

    SLIDE

--- src/app/app.js
+++ src/app/app.js
@@ -22,59 +22,19 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 // HEADER
-var cont = 0;
-var xx;
+let currentSlide = 1;
+        const totalSlides = 5;
 
-function loopSlider() {
-    xx = setInterval(function() {
-        switch (cont) {
-            case 0:
-                {
-                    $("#slider-1").fadeOut(400);
-                    $("#slider-2").delay(400).fadeIn(400);
-                    $("#sButton1").removeClass("bg-purple-800");
-                    $("#sButton2").addClass("bg-purple-800");
-                    cont = 1;
-                    break;
-                }
-            case 1:
-                {
-                    $("#slider-2").fadeOut(400);
-                    $("#slider-1").delay(400).fadeIn(400);
-                    $("#sButton2").removeClass("bg-purple-800");
-                    $("#sButton1").addClass("bg-purple-800");
-                    cont = 0;
-                    break;
-                }
+        function showSlide(slideNumber) {
+            document.querySelectorAll('.slide').forEach(slide => {
+                slide.style.display = 'none';
+            });
+            document.getElementById(`slide${slideNumber}`).style.display = 'block';
         }
-    }, 8000);
-}
 
-function reinitLoop(time) {
-    clearInterval(xx);
-    setTimeout(loopSlider(), time);
-}
+        function nextSlide() {
+            currentSlide = currentSlide % totalSlides + 1;
+            showSlide(currentSlide);
+        }
 
-function sliderButton1() {
-    $("#slider-2").fadeOut(400);
-    $("#slider-1").delay(400).fadeIn(400);
-    $("#sButton2").removeClass("bg-purple-800");
-    $("#sButton1").addClass("bg-purple-800");
-    reinitLoop(4000);
-    cont = 0;
-}
-
-function sliderButton2() {
-    $("#slider-1").fadeOut(400);
-    $("#slider-2").delay(400).fadeIn(400);
-    $("#sButton1").removeClass("bg-purple-800");
-    $("#sButton2").addClass("bg-purple-800");
-    reinitLoop(4000);
-    cont = 1;
-}
-
-$(window).ready(function() {
-    $("#slider-2").hide();
-    $("#sButton1").addClass("bg-purple-800");
-    loopSlider();
-});
+        setInterval(nextSlide, 3000); // Cambiar de slide cada 3 segundos
